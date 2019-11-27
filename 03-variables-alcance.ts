47a06e67c9f0b6ac3c87e14adca0d19aac04cd89
David Rivera <alberto.rivera@epn.edu.ec>
2019-11-27 15:06:21 -0500

    splice

---
+++ 03-variables-alcance.ts
@@ -0,0 +1,56 @@
+let edad=30;
+if (edad>18){
+    console.log("Legal")
+}
+else{
+    console.log("a la carcel por pedofilo")
+}
+////
+////
+
+if (true)//Bloque 1
+{
+    let edad =30;
+    console.log(edad);
+    if (edad)//Bloque 2
+    {
+        console.log(edad);
+        if(edad){//Bloque 3
+            console.log(edad);
+        }
+        else{//Bloque 4
+            console.log(edad);
+        }
+
+    }
+    else{//Bloque 5
+        console.log(edad);
+    }
+
+}
+
+else{//Bloque 6
+    console.log(edad);
+
+}
+///
+///
+for(let i=0;i>10;i++)
+{
+    let bandera=0;
+    bandera=bandera+i;
+    console.log()
+}
+///
+///
+if(true)
+{
+    for(let i=0;i>10;i++)
+  {
+    let bandera=0;
+    bandera=bandera+i;
+    console.log()
+  }
+}
+/// los bloque de codigo nos ayuda a definir las variables
+
---
+++ Tareas Noviembre/Programar-splice.ts
@@ -0,0 +1,16 @@
+const arregloNumeros=[1,2,3,4,5,6,7,8,9,10];
+//Acceder 
+//Necesito:Indice
+//arregloNumeros.push(11);//7
+//arregloNumeros.pop();
+//console.log(arregloNumeros)
+/*arregloNumeros.splice(0,1+1);
+console.log(arregloNumeros);*/
+/*arregloNumeros.splice(6);
+console.log(arregloNumeros);*/
+//arregloNumeros.indexOf(5);
+//console.log(arregloNumeros.indexOf(9));
+if(arregloNumeros.indexOf(5)<6)
+{
+    console.log(arregloNumeros);
+}
